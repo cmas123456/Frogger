a576fd31409bc8b900ebe9c4f7d5a02b0cfcddc8
Chris Masters <cmas123456@gmail.com>
2019-12-02 09:39:45 -0800

    basic frog2.0

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const source = document.createElement('canvas')
+function assignAttributes(element, attributes) {
+	Object.keys(attributes).forEach(key => element.setAttribute(key, attributes[key]))
+}
 assignAttributes(source, {
   id: 'source',
 	height: window.innerHeight - 20,
@@ -6,4 +9,6 @@ assignAttributes(source, {
 })
 document.body.appendChild(source)
 const context = source.getContext('2d', {alpha: 'false'})
+
+context.fillStyle = 'green';
 context.fillRect(frog.origin[0],frog.origin[1],frog.dimensions[0],frog.dimensions[1]);
