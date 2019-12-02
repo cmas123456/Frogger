33a4df4bf22ae9f3e4fb446f56f639217135e7a0
Richard <richardwjmorrow@gmail.com>
2019-12-02 10:48:12 -0800

    water won't display yet

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ document.body.appendChild(source)
 const context = source.getContext('2d', {alpha: 'false'})
 
 context.fillStyle = 'blue';
-context.fillRect =(water.origin[0],water.origin[1],water.dimensions[0], water.dimensions[1]);
+context.fillRect(water.origin[0],water.origin[1],water.dimensions[0],water.dimensions[1]);
+
 context.fillStyle = 'green';
 context.fillRect(frog.origin[0],frog.origin[1],frog.dimensions[0],frog.dimensions[1]);
