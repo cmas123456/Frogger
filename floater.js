0feca9b88f00d8ad8b3edb7411d70d39a920fc1f
Richard <richardwjmorrow@gmail.com>
2019-12-02 11:05:27 -0800

    water works now

--- floater.js
+++ floater.js
@@ -1,9 +1,9 @@
 let water = {};
 let waterCreate = (() => {
     water = {
-        origin = [0, window.innerHeight / 10],
-        dimensions =[window.innerWidth, window.innerHeight / 40],
-        image = null,
-        death = true
+        origin: [0, window.innerHeight / 10],
+        dimensions: [window.innerWidth, window.innerHeight * .40],
+        image: null,
+        death: true
     }
 })();
