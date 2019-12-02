62e4a6400828a0c3b329c2348bfbe41bfb3ca0d8
Chris Masters <cmas123456@gmail.com>
2019-12-02 10:23:32 -0800

    added basic movement

--- main.js
+++ main.js
@@ -10,5 +10,27 @@ assignAttributes(source, {
 document.body.appendChild(source)
 const context = source.getContext('2d', {alpha: 'false'})
 
+function drawBackground() {
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    context.fillStyle = 'blue';
+    context.fillRect(0, 0, window.innerWidth, window.innerHeight / 2);
+    context.fillStyle = 'brown';
+    context.fillRect(0, 0, window.innerWidth, window.innerHeight * .1);
+  }
+
+function drawObjects () {
+    drawBackground();
+    frog.Draw();
+    frog.Move();
+
+}
 context.fillStyle = 'green';
 context.fillRect(frog.origin[0],frog.origin[1],frog.dimensions[0],frog.dimensions[1]);
+
+let gameLoop = (() => {
+    const gameLoop = setInterval(() => {
+        drawObjects();
+    },1000/60)
+  })()
