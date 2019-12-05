0cf7cf6a66bf8fdc12901bb99e89b103d0493698
vivien-renee <vivien.renee.b@gmail.com>
2019-12-05 11:34:26 -0800

    speed and spacing adjusted: no blank spaces on road when game starts

--- road.js
+++ road.js
@@ -11,50 +11,64 @@
       context.fillRect(this.x, this.y, this.h, this.w)
     } 
   }
-  ////Lane1 Cars
 
-  let car1 = new Car(1, 2, 1, 1, 'red', -1)
-  let car2 = new Car(1.5, 2, 2, 1, 'red', -1)
-  let car3 = new Car(2, 2, 1, 1, 'red', -1)
+  let lane1 = []
+  let lane2 = []
+  let lane3 = []
+  let lane4 = []
+  let hov = []
+  ////Lane1 Cars
+  lane1.push(new Car(0, 1, 2, 1, 'red', -1.5))
+  lane1.push(new Car(.5, 1, 2, 1, 'red', -1.5))
+  lane1.push(new Car(1, 1, 2, 1, 'red', -1.5))
+  lane1.push(new Car(1.5, 1, 2, 1, 'red', -1.5))
+  lane1.push(new Car(2.5, 1, 2, 1, 'red', -1.5))
+  lane1.push(new Car(3, 1, 2, 1, 'red', -1.5))
   ///lane2 Cars
-  let car4 = new Car(1, 3, 1, 1, 'blue', 2)
-  let car5 = new Car(1.5, 3, 1, 1, 'blue', 2)
-  let car6 = new Car(2, 3, 1, 1, 'blue', 2)
+  lane2.push(new Car(0, 2, 2, 1, 'blue', 2))
+  lane2.push(new Car(1, 2, 2, 1, 'blue', 2))
+  lane2.push(new Car(1.5, 2, 2, 1, 'blue', 2))
+  lane2.push(new Car(2, 2, 2, 1, 'blue', 2))
   ///lane3 cars
-  let car7 = new Car(1, 4, 1, 1, 'green', -2)
-  let car8 = new Car(1.5, 4, 2, 1, 'green', -2)
-  let car9 = new Car(2, 4, 3, 1, 'green', -2)
-  let abc779 = new Car(2.5, 4, 3, 1, 'green', -2)
-   ///lane4 cars
-  let car10 = new Car(1, 5, 1, 1, 'yellow',3)
-  let car11 = new Car(1.5, 5, 3, 1, 'yellow', 3)
-  let car12 = new Car(2, 5, 1, 1, 'yellow', 3)
-   ///hov cars
-  let car13 = new Car(1, 1, 1, 1, 'orange', -5)
-  let car14 = new Car(1.5, 1, 1, 1, 'orange', -5)
-  let car15 = new Car(3, 1, 5, 1, 'orange', -5)
+  lane3.push(new Car(.1, 3, 2, 1, 'green', -2))
+  lane3.push(new Car(.5, 3, 2, 1, 'green', -2))
+  lane3.push(new Car(1, 3, 2, 1, 'green', -2))
+  lane3.push(new Car(1.5, 3, 2, 1, 'green', -2))
+  lane3.push(new Car(2, 3, 2, 1, 'green', -2))
+  lane3.push(new Car(3, 3, 2, 1, 'green', -2))
   
-  let lane1 = [car1, car2, car3]
-  let lane2 = [car4, car5, car6]
-  let lane3 = [car7, car8, car9, abc779]
-  let lane4 = [car10, car11, car12]
-  let hov = [car13, car14, car15]
+   ///lane4 cars
+  lane4.push(new Car(0, 4, 2, 1, 'yellow',3))
+  lane4.push(new Car(1, 4, 2, 1, 'yellow',3))
+  lane4.push(new Car(1.5, 4, 2, 1, 'yellow', 3))
+  lane4.push(new Car(2, 4, 2, 1, 'yellow', 3))
+   ///hov cars
+  hov.push(new Car(0, 5, 5, 1, 'orange', -4.75))
+  hov.push(new Car(1.1, 5, 5, 1, 'orange', -4.75))
+  hov.push(new Car(1.8, 5, 5, 1, 'orange', -4.75))
+  hov.push(new Car(2.5, 5, 5, 1, 'orange', -4.75)) 
+  hov.push(new Car(3.2, 5, 5, 1, 'orange', -4.75))
+  
+  
   let road = [lane1, lane2, lane3, lane4, hov]
  
   
   function goZoom () {
     road.forEach(lane => 
       lane.forEach(car => {
+        //right to left driving cars
       if(car.speed < 1){
           if(car.x > -300)//800 is the end point
          {car.x += car.speed}//1 is the speed
       else{
         
-        car.x = 900//-50 is respawn point
+        car.x = 2000//-50 is respawn point
       }
+      ///
          }
+         ////right to left cars
       else{
-         if(car.x < 800)//800 is the end point
+         if(car.x < 2000)//800 is the end point
          {car.x += car.speed}//1 is the speed
          else{
         
